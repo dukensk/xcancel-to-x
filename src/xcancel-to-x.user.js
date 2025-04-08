48d7e254708fcbf175befcbcc76c88b6f1905255
duke <duke.nsk@gmail.com>
2025-04-08 22:43:07 +0700

    add: icon and math to xcancel.com

--- src/xcancel-to-x.user.js
+++ src/xcancel-to-x.user.js
@@ -6,8 +6,9 @@
 // @version            1.00
 // @run-at             document-start
 // @author             duke.nsk@gmail.com
+// @match              https://xcancel.com/*
+// @icon               https://www.google.com/s2/favicons?sz=64&domain=x.com
 // @grant              none
 // ==/UserScript==
 
 window.location.replace("https://x.com" + window.location.pathname + window.location.search);
-
